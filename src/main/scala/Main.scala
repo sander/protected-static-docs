5a4352f70d17ea63986ea0929e7e2bfbe22d0f18
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-01-16 13:02:16 +0100

    feat: add content-length and etag

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,7 +9,7 @@ import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.dsl.io.*
 import org.http4s.*
 import org.http4s.server.Router
-import org.http4s.headers.`Content-Type`
+import org.http4s.headers.*
 import org.http4s.headers.Location
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
@@ -21,29 +21,34 @@ import scala.concurrent.ExecutionContext.global
 object Main extends IOApp.Simple:
   val port = 8080
   val entryPoint = uri"/index.html"
-  case object ContentTypeError extends Exception("could not parse content type")
+  case object HeaderParserError extends Exception("could not parse header")
+  private def header[T](result: ParseResult[T]): IO[T] = IO(
+    result.getOrElse(throw HeaderParserError)
+  )
   def reverseProxyService(getObject: GetObject) = HttpRoutes.of[IO] {
     case GET -> Root => Found().map(_.putHeaders(Location(entryPoint)))
-    case GET -> path =>
+    case request @ GET -> path =>
       for
-        response <- EitherT(
+        r <- EitherT(
           getObject(
             GetObjectRequest
               .builder()
               .bucket("test-static-docs")
               .key(path.segments.mkString("/"))
+              .ifNoneMatch(request.headers.get[`If-None-Match`] match {
+                case Some(h) => h.toString
+                case None    => null
+              })
               .build()
           )
         ).valueOrF(IO.raiseError)
-        cty <- IO(
-          `Content-Type`
-            .parse(response.properties.contentType())
-            .getOrElse(throw ContentTypeError)
-        )
+        cty <- header(`Content-Type`.parse(r.properties.contentType()))
+        cl <- header(`Content-Length`.fromLong(r.properties.contentLength()))
+        etag <- header(ETag.parse(r.properties.eTag()))
       yield Response(
         Status.Ok,
-        body = response.body,
-        headers = Headers(cty)
+        body = r.body,
+        headers = Headers(cty, cl, etag)
       )
   }
   private def httpApp(getObject: GetObject): HttpApp[IO] = Router(
